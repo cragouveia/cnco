23817be9c01ee936f1bd44cf6e1b9fb76dc12ce8
cragouveia <mcdia$01>
2017-09-14 15:14:36 -0400

    Correção na geração do link.

--- src/main/java/br/com/quantati/cnco/CncoApplication.java
+++ src/main/java/br/com/quantati/cnco/CncoApplication.java
@@ -1,9 +1,15 @@
 package br.com.quantati.cnco;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@EnableCaching
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class CncoApplication {
 
 	public static void main(String[] args) {
